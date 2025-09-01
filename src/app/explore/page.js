92efabebaf6c2d703e7ef637e69bc4c4452a3900
Romil Shah <romil@Romils-MacBook-Pro.local>
2025-09-02 01:38:32 +0530

    fix 1

--- src/app/explore/page.js
+++ src/app/explore/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ExplorePage from "@/components/explore/components";
 
 export default function Page() {
-    return <ExplorePage />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <ExplorePage />
+        </Suspense>
+    );
 } 
