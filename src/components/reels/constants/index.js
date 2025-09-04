7b1f78de4b3a85030bab9e97e6a9ed8bf676770c
dipesh416 <dipeshdeepgothi416@gmail.com>
2025-09-05 01:10:13 +0530

    add reels

--- src/components/reels/constants/index.js
+++ src/components/reels/constants/index.js
@@ -1,22 +1,34 @@
+import { reel1, reel2, reel3, reel4, reel5 } from "@/assets/mp4";
+
 export const REELS_DATA = [
   {
     id: 1,
     title: "Ladakh Adventure",
     caption: "Dusty roads, high passes, and endless skies.",
-    src: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    src: reel1,
   },
   {
     id: 2,
     title: "Bali Vibes",
     caption: "Sunsets and swings over rice terraces.",
-    src: "https://www.w3schools.com/howto/rain.mp4",
+    src: reel2,
   },
   {
     id: 3,
     title: "Vietnam Streets",
     caption: "Scooters, coffee, and lantern-lit nights.",
-    src: "https://www.w3schools.com/howto/rain.mp4",
+    src: reel3,
+  },
+  {
+    id: 4,
+    title: "Maldives Paradise",
+    caption: "Clear waters, white sands, and crystal-clear skies.",
+    src: reel4,
+  },
+  {
+    id: 5,
+    title: "Thailand Beach",
+    caption: "Sunsets, beaches, and tropical vibes.",
+    src: reel5,
   },
 ];
-
-
