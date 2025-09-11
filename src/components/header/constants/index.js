6a771ab7fa46083648778ec3fbf0f68ef1077cb2
Romil Shah <romil@Romils-MacBook-Pro.local>
2025-09-11 16:37:09 +0530

    about us removed

--- src/components/header/constants/index.js
+++ src/components/header/constants/index.js
@@ -59,7 +59,7 @@ export const DOMESTIC_WEEKEND_GETAWAYS = {
 // More dropdown links
 export const MORE_LINKS = [
   { label: "Blogs", href: "/blogs" },
-  { label: "About us", href: "/about" },
+  // { label: "About us", href: "/about" },
   // { label: "Refer a friend", href: "/refer" },
   // { label: "Careers", href: "/careers" },
 ];
